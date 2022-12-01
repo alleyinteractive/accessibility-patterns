05b4ec8152a0e20fe5e47ef19eb5727620338a54
Kaitlin Bolling <kaitlin@alley.com>
2022-12-01 13:01:53 -0500

    add accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     '@storybook/addon-interactions',
     'storybook-addon-turbo-build',
     '@storybook/preset-scss',
+    '@storybook/addon-a11y',
   ],
   framework: '@storybook/react',
   core: {
