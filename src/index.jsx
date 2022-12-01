fcc3b85f9bb111e82519c6f31841ee34ffcd49fd
Kaitlin Bolling <kaitlin@alley.com>
2022-12-01 12:49:03 -0500

    reorganize components, remove examples

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 function App() {
   return (
     <>
       <h1>Accessibility Patterns</h1>
       <p>A library of accessible web components and patterns.</p>
+      <p><a href="https://main--635823548f5047fa4bb4ee22.chromatic.com">View Library</a></p>
     </>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = createRoot(document.getElementById('app'));
+root.render(<App tab="home" />);
