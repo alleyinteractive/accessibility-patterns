e08c915d4aafda134fe476ea5a600516e9fdb734
Matthew Ell <matt.ell@alley.co>
2022-12-27 15:52:22 -0500

    Add Breadcrumb component

---
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './breadcrumb.scss';
+
+/**
+ * Breadcrumb component.
+ */
+const Breadcrumb = ({
+  items,
+}) => (
+  <nav aria-label="breadcrumb" className="storybook-breadcrumb">
+    <ol>
+      {items.map(({ title, url }, index) => (
+        <li key={title}><a href={url} aria-current={items.length === index + 1 ? 'page' : null}>{title}</a></li>
+      ))}
+    </ol>
+  </nav>
+);
+
+Breadcrumb.propTypes = {
+  /**
+   * Breadcrumb items in hierarchical order with current/active page
+   * appearing last.
+   */
+  items: PropTypes.arrayOf(PropTypes.shape({
+    /**
+     * Title of breadcrumb link.
+     */
+    title: PropTypes.string.isRequired,
+    /**
+     * URL of breadcrumb link.
+     */
+    url: PropTypes.string.isRequired,
+  })),
+};
+
+Breadcrumb.defaultProps = {
+  items: [],
+};
+
+export default Breadcrumb;
