f8db302523db0393d0e22c708978a33690d35cfd
Jessica Goddard <jessica@alley.co>
2024-01-31 08:27:00 -0500

    Sets up keyboard functionality

--- src/components/hamburger-menu/HamburgerMenu.jsx
+++ src/components/hamburger-menu/HamburgerMenu.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import './hamburgerMenu.scss';
 
 /**
  * Hamburger menu component.
@@ -7,36 +8,160 @@ import PropTypes from 'prop-types';
 const HamburgerMenu = ({
   menuItems,
 }) => {
-  const [isExpanded, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const mainMenuRef = useRef();
+  const hamburgerMenuButtonRef = useRef();
+  const firstMenuItemRef = useRef();
+  const lastMenuItemRef = useRef();
+  const menuItemsRef = useRef();
 
-  const handleToggle = () => {
-    setIsOpen(!isExpanded);
+  // Update refs when menu items change.
+  useEffect(() => {
+    menuItemsRef.current = mainMenuRef.current.querySelectorAll('li a');
+    firstMenuItemRef.current = mainMenuRef.current.querySelector('li:first-child a');
+    lastMenuItemRef.current = mainMenuRef.current.querySelector('li:last-child a');
+  }, [mainMenuRef]);
+
+  // Closes the menu and removes event listener.
+  const closeMenu = () => {
+    setIsOpen(false);
+    hamburgerMenuButtonRef.current.focus();
+    // eslint-disable-next-line no-use-before-define
+    mainMenuRef.current.removeEventListener('keydown', handleMenuItemKeyDown);
   };
+
+  // Opens the menu and adds menu event listener.
+  const openMenu = () => {
+    setIsOpen(true);
+    // eslint-disable-next-line no-use-before-define
+    mainMenuRef.current.addEventListener('keydown', handleMenuItemKeyDown);
+  };
+
+  // Toggles menu open/closed when user clicks menu button.
+  const handleMenuToggle = () => {
+    if (isOpen === true) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  };
+
+  // Handles key events on menu button.
+  // Enter or Space: toggle menu open/closed.
+  // Escape: close menu.
+  // ArrowDown: open menu and focus on first menu item.
+  // ArrowUp: open menu and focus on last menu item.
+  const handleMenuButtonKeyDown = (event) => {
+    switch (event.key) {
+      case 'Enter':
+      case ' ':
+        event.preventDefault();
+        handleMenuToggle();
+        break;
+      case 'Escape':
+        closeMenu();
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        openMenu();
+        firstMenuItemRef.current.focus();
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        openMenu();
+        lastMenuItemRef.current.focus();
+        break;
+      default:
+        break;
+    }
+  };
+
+  // Handles key events on menu.
+  // Escape or Tab: close menu.
+  // ArrowDown: focus on next menu item.
+  // ArrowUp: focus on previous menu item.
+  // Home: focus on first menu item.
+  // End: focus on last menu item.
+  const handleMenuItemKeyDown = (event) => {
+    const currentMenuItem = event.target;
+    const currentIndex = Array.prototype.indexOf.call(menuItemsRef.current, currentMenuItem);
+    let nextIndex = currentIndex;
+
+    switch (event.key) {
+      case 'Escape':
+      case 'Tab':
+        closeMenu();
+        return;
+      case 'ArrowDown':
+        nextIndex += 1;
+        break;
+      case 'ArrowUp':
+        nextIndex -= 1;
+        break;
+      case 'Home':
+        event.preventDefault();
+        firstMenuItemRef.current.focus();
+        return;
+      case 'End':
+        event.preventDefault();
+        lastMenuItemRef.current.focus();
+        return;
+      default:
+        return;
+    }
+
+    event.preventDefault();
+
+    // Loop to start or end if index is out of bounds.
+    if (nextIndex < 0) {
+      nextIndex = menuItemsRef.current.length - 1;
+    } else if (nextIndex >= menuItemsRef.current.length) {
+      nextIndex = 0;
+    }
+
+    menuItemsRef.current[nextIndex].focus();
+  };
+
+  // Adds event listener for menu button keydown.
+  useEffect(() => {
+    const hamburgerMenuButton = hamburgerMenuButtonRef.current;
+    // Add event listener for menu button keydown.
+    hamburgerMenuButton.addEventListener('keydown', handleMenuButtonKeyDown);
+    return () => {
+      // Remove event listener when this component is unmounted.
+      hamburgerMenuButton.removeEventListener('keydown', handleMenuButtonKeyDown);
+    };
+  });
 
   return (
     <nav
       role="navigation"
-      aria-label="Hamburger Menu"
+      aria-label="Main Menu"
       className="hamburger-menu-container"
+      ref={mainMenuRef}
     >
       <button
         type="button"
         id="hamburger-menu-button"
-        aria-haspopup="true"
+        className="hamburger-menu-button"
+        aria-haspopup="menu"
         aria-controls="hamburger-menu"
-        aria-expanded={isExpanded}
-        onClick={handleToggle}
+        aria-expanded={isOpen}
+        onClick={handleMenuToggle}
+        ref={hamburgerMenuButtonRef}
       >
         Menu
       </button>
       <ul
         id="hamburger-menu"
+        className="hamburger-menu"
         aria-labelledby="hamburger-menu-button"
         role="menu"
       >
-        {menuItems.map(({ title, url }) => (
+        {menuItems.map(({ title, url }, key) => (
           <li
             role="none"
+            className={`item-${key + 1}`}
             key={title}
           >
             <a
